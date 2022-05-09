85765d3f57cbac281a1bd23054119d992bc39bd4
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-05-09 20:18:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let myResume={
+    "basics": {
+      "name": "AMARNATH  J",
+      "email": "j1999amar@gamil.com",
+      "phone": 8098385972,
+      "degree": "MCA",
+      "location": {
+        "address": "NO.6 NGO COLONY,KEERANUR,PUDUKKOTTAI",
+        "postalCode": 622502,
+        "city": "PUDUKKOTTAI",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    },
+    
+    "education": [
+      {
+        "institution": "JAMAL MOHAMMED COLLEGE,TRICHY",
+        "department": "MCA",
+        "studyType": "CS",
+        "batch start year": 2017,
+        "batch end year": 2020,
+        "gpa": 6.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
